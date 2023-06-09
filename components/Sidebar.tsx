8dfea243c8fc7b8dc47ad6b26f3f1eff15b6d74c
nishchal <singhnishchal27@gmail.com>
2023-06-09 09:52:47 +0530

    new component - 'added box file'

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,9 @@
 
 import {usePathname} from 'next/navigation';
 import { useMemo } from 'react';
+import {HiHome} from "react-icons/hi";
+import {BiSearch} from "react-icons/bi";
+import Box from './Box';
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -11,11 +14,13 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
     const pathname = usePathname();
     const routes = useMemo(() => [
         {
+            icon: HiHome,
             label: 'Home',
             active: pathname !== '/search',
             href: '/',
         },
         {
+            icon: BiSearch,
             label: 'Search',
             active: pathname === '/search',
             href: '/search'
@@ -23,7 +28,13 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
     ], [pathname]);
 
     return (
-        <div>{children}</div>
+        <div className="flex h-full">
+            <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+                <Box>
+                    sidebar navigation
+                </Box>
+            </div>
+        </div>
     );
 }
 
