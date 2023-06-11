4e95c63ac8b6d8c36c68f794ee830fd5fb5e8a5a
nishchal <singhnishchal27@gmail.com>
2023-06-11 17:26:14 +0530

    new component - 'added Header file'

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import {usePathname} from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useMemo } from 'react';
-import {HiHome} from "react-icons/hi";
-import {BiSearch} from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 import SidebarItem from './SidebarItem';
 import Box from './Box';
+import Library from './Library';
 
 interface SidebarProps {
     children: React.ReactNode;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({children}) => {
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     const pathname = usePathname();
     const routes = useMemo(() => [
         {
@@ -33,15 +34,18 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
             <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
                 <Box>
                     <div className="flex flex-col gap-y-4 px-5 py-4">
-                        {routes.map((item) =>(
+                        {routes.map((item) => (
                             <SidebarItem key={item.label} {...item} />
                         ))}
                     </div>
                 </Box>
                 <Box className="overflow-y-auto h-full">
-                    song Library
+                    <Library/>
                 </Box>
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
         </div>
     );
 }
