329a98ccfef83d6e4cdb66fd6efc2f9e6c509cae
nishchal <singhnishchal27@gmail.com>
2023-06-19 21:31:25 +0530

    reusable component - 'added button component'

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,10 @@
 
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
-import {RxCaretLeft} from "react-icons/rx";
+import {RxCaretLeft, RxCaretRight} from "react-icons/rx";
+import {HiHome} from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -20,9 +23,29 @@ const Header: React.FC<HeaderProps> = ({children, className}) => {
     className)}>
       <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
-          <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+          <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          onClick={() => router.back()}
+          >
             <RxCaretLeft size={35} className="text-white"/>
           </button>
+          <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          onClick={() => router.forward()}
+          >
+            <RxCaretRight size={35} className="text-white"/>
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-betweeen items-center gap-x-4">
+          <div>
+            <Button>Sign up</Button>
+          </div>
         </div>
       </div>
     </div>
