81c04705df54a1f92ff68c88cb73406b2958b71b
nishchal <singhnishchal27@gmail.com>
2023-09-18 18:33:35 +0530

    features - like favourities list update

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -4,9 +4,10 @@ import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 import { useSessionContext } from "@supabase/auth-helpers-react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 interface LikeButtonProps {
   songId: string;
@@ -43,7 +44,46 @@ const LikeButton: React.FC<LikeButtonProps> = ({ songId }) => {
 
   const Icon = isLiked ? AiFillHeart : AiOutlineHeart;
 
-  return <div>LikeButton</div>;
+  //"like" relevant db is updated using song_id and user_id
+  const handleLike = async () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    if (isLiked) {
+      const { error } = await supabaseClient
+        .from("liked_songs")
+        .delete()
+        .eq("user_id", user.id)
+        .eq("song_id", songId);
+
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(false);
+      }
+    } else {
+      const { error } = await supabaseClient.from("liked_songs").insert({
+        song_id: songId,
+        user_id: user.id,
+      });
+
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(true);
+        toast.success("Liked!");
+      }
+    }
+
+    router.refresh();
+  };
+
+  return (
+    <button className="hover:opacity-75 transition" onClick={handleLike}>
+      <Icon color={isLiked ? "#22c55e" : "white"} size={25} />
+    </button>
+  );
 };
 
 export default LikeButton;
